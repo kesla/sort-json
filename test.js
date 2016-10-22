c5bf218ffe48ecbe746faebe13cfcc77605289cc
oprogramador <uomodislesia@gmail.com>
2016-10-22 11:25:45 +0200

    simple array and object tests

--- test.js
+++ test.js
@@ -9,4 +9,17 @@ describe('visit', () => {
   it('returns undefined for undefined', () => {
     expect(visit(undefined)).to.be.undefined();
   });
+
+  it('does not change array order', () => {
+    const givenData = [1, 5, 2, -1, 3];
+
+    expect(JSON.stringify(visit(givenData))).to.equal(JSON.stringify(givenData));
+  });
+
+  it('sorts object by keys', () => {
+    const givenData = { foo: 123, bar: 456, baz: 789};
+    const expectedData = { bar: 456, baz: 789, foo: 123};
+
+    expect(JSON.stringify(visit(givenData))).to.equal(JSON.stringify(expectedData));
+  });
 });
