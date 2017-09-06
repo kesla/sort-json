65c16cddd80eea32bdd009080a16e34f9e690475
danielyaa5 <danielyaa5@gmail.com>
2017-09-06 16:20:25 -0700

    reduce cmd file code

--- cmd.js
+++ cmd.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
+/* eslint-disable no-use-before-define */
+
 // Core dependencies
-var fs = require('fs');
 var path = require('path');
 var _ = require('lodash');
 
 // NPM dependencies
-var detectIndent = require('detect-indent');
 var sortJson = require('./');
 
 // Get all the files
@@ -14,40 +14,9 @@ var files = process.argv.slice(0).filter(arg => arg.endsWith('.json') || arg.end
 var ignoreCase = _.includes(process.argv, '--ignore-case') || _.includes(process.argv, '-i');
 var reverse = _.includes(process.argv, '--reverse') || _.includes(process.argv, '-r');
 
-files.forEach(readEachFile);
-
-function readEachFile(fileName) {
-  var filePath = path.resolve(fileName);
-  fs.readFile(filePath, 'utf8', readingFile);
-
-  function readingFile(err, file) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-
-    // Detect indentation if none is detectable fall back to two spaces
-    var indent = detectIndent(file).indent || '  ';
-    // Detect end of line if any
-    var eol = /}(\r\n|\n|\r)$/.exec(file, 'gm');
-
-    // Parse JSON
-    var json;
-    try {
-      json = JSON.parse(file);
-    } catch(err) {
-      return console.error(err + ' => ' + filePath);
-    }
-
-    // Sorting
-    var sortedObject = sortJson(json, { ignoreCase: ignoreCase, reverse: reverse });
-
-    // Saving to file
-    fs.writeFile(filePath, JSON.stringify(sortedObject, null, indent) + ((eol && eol.length === 2) ? eol[1] : ''), function(err) {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-    });
-  }
-}
+sortJson(
+  files.map(function (f) {
+    return path.resolve(f);
+  }),
+  { ignoreCase, reverse, overwrite: true }
+);
