ce289586178612dd2d288bf1357470c84e9fd361
Christian Schuller <cschuller@servusalps.com>
2018-06-09 08:07:53 +0200

    Enables eslint and fixes violations

--- app/cmd.js
+++ app/cmd.js
@@ -3,22 +3,20 @@
 /* eslint-disable no-use-before-define */
 
 // Core dependencies
-var path = require('path');
-var _ = require('lodash');
+const path = require('path');
+const _ = require('lodash');
 
 // NPM dependencies
-var sortJson = require('./');
+const sortJson = require('./');
 
 // Get all the files
-var files = process.argv.slice(0).filter(arg => arg.endsWith('.json') || arg.endsWith('.rc'));
-var ignoreCase = _.includes(process.argv, '--ignore-case') || _.includes(process.argv, '-i');
-var reverse = _.includes(process.argv, '--reverse') || _.includes(process.argv, '-r');
-var dirtyDepth = process.argv.slice(0).filter(arg => arg.startsWith('-d') || arg.startsWith('--depth'));
-var depth = dirtyDepth.length > 0 ? parseInt(dirtyDepth[0].split('=')[1], 10) : Infinity;
+const files = process.argv.slice(0).filter(arg => arg.endsWith('.json') || arg.endsWith('.rc'));
+const ignoreCase = _.includes(process.argv, '--ignore-case') || _.includes(process.argv, '-i');
+const reverse = _.includes(process.argv, '--reverse') || _.includes(process.argv, '-r');
+const dirtyDepth = process.argv.slice(0).filter(arg => arg.startsWith('-d') || arg.startsWith('--depth'));
+const depth = dirtyDepth.length > 0 ? parseInt(dirtyDepth[0].split('=')[1], 10) : Infinity;
 
 sortJson.overwrite(
-  files.map(function (f) {
-    return path.resolve(f);
-  }),
+  files.map(file => path.resolve(file)),
   { ignoreCase, reverse, depth }
 );
